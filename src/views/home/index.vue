33e11631e62997002395a3fb3caabf8f149192c9
amice5200 <839589736@qq.com>
2019-09-02 15:39:27 +0800

    后台首页大体容器布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
+  <el-container class="my-container">
+    <!-- 左边侧栏 -->
+    <el-aside width="200px" class="my-aside">Aside</el-aside>
+    <!-- 右边内容区 -->
+    <el-main class="my-main">
+      <el-container>
+        <!-- 右边的头部 -->
+        <el-header class="main-header">Header</el-header>
+        <!-- 右边内容区 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-main>
   </el-container>
 </template>
 
@@ -9,4 +18,20 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="less">
+.my-container {
+  height: 100%;
+
+  .my-aside {
+    background-color: #323745;
+  }
+
+  .my-main {
+    padding: 0;
+
+    .main-header {
+      background-color: hotpink;
+    }
+  }
+}
+</style>
